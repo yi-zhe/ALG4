0119f7d07e04d38bc087dea044b0e5768b482fed
l <l@com>
2023-05-18 20:26:26 +0800

    归并排序-自顶向下

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -1,18 +1,43 @@
 package sort;
 
+import utils.SortUtils;
+
 import static utils.SortUtils.isSorted;
 import static utils.SortUtils.show;
 
 public class MergeSort {
 
     public static void sort(Comparable[] a) {
-        
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + ((hi - lo) >> 1);
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (SortUtils.less(aux[j], aux[i])) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
     }
 
     public static void main(String[] args) {
         Comparable[] a = {2, 3, 4, 1, 6};
         sort(a);
-        assert isSorted(a);
+        System.out.println(isSorted(a));
         show(a);
     }
 }
