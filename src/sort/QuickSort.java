0fb2b79d67c393a26bc975f6a7052bb4075ac736
l <l@com>
2023-05-19 17:58:08 +0800

    快速排序

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,12 +1,31 @@
 package sort;
 
-import static utils.SortUtils.isSorted;
-import static utils.SortUtils.show;
+import static utils.SortUtils.*;
 
 public class QuickSort {
 
     public static void sort(Comparable[] a) {
-        
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1; // 左右扫描指针
+        Comparable v = a[lo]; // 切分元素
+        while (true) { // 扫描左右，检查扫描是否结束并交换元素
+            while (less(a[++i], v)) if (i == hi) break;
+            while (less(v, a[--j])) if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
     }
 
     public static void main(String[] args) {
